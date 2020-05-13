aa5b79486e527408c9cb9f3b46245c2c6c8d25c3
topurple <top2urple@foxmail.com>
2020-05-13 20:24:39 +0800

    Site updated: 2020-05-13 20:24:34

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Wed May 13 2020 20:24:18 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
